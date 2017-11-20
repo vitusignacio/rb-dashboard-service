e6797b337c93a2f6cab1c4e97dcda692697b523a
vitusignacio <33056389+vitusignacio@users.noreply.github.com>
2017-11-21 01:53:06 +0700

    Added redis support.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,12 +8,16 @@ class UserController < ApplicationController
   end
 
   def add
-    user = User.new
-    user.userId = rand(0...9999)
-    user.name = params[:name]
-    user.title = params[:title]
-    user.age = params[:age].to_i
-    user.save!
+    # user = User.new
+    # user.userId = rand(0...9999)
+    # user.name = params[:name]
+    # user.title = params[:title]
+    # user.age = params[:age].to_i
+    # user.save!
+
+    logger.info { 'broadcasting...' }
+    redis = Redis.new(host: '127.0.0.1', port: 6379, db: 15)
+    redis.publish('user-created', 'test')
 
     render json: { status: 'ok' }
   end
